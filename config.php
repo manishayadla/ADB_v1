f42b543d85b382581ae870628326115d5fd7b689
manishayadla <31947122+manishayadla@users.noreply.github.com>
2022-12-08 00:42:56 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,65 +1,25 @@
-namespace Google\Cloud\Samples\CloudSQL\MySQL;
-
-use PDO;
-use PDOException;
-use RuntimeException;
-use TypeError;
-
-class DatabaseUnix
-{
-    public static function initUnixDatabaseConnection(): PDO
-    {
-        try {
-            // Note: Saving credentials in environment variables is convenient, but not
-            // secure - consider a more secure solution such as
-            // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-            // keep secrets safe.
-            $username = "manisha"; // e.g. 'your_db_user'
-            $password = "123"; // e.g. 'your_db_password'
+<?php
+$username = "manisha"; // e.g. 'your_db_user'
+           $password = "123"; // e.g. 'your_db_password'
             $dbName = "hospitalmanagement"; // e.g. 'your_db_name'
             $instanceUnixSocket = "/cloudsql/adb-yadlamanisha:us-central1:manishayadla"; // e.g. '/cloudsql/project:region:instance'
 
-            // Connect using UNIX sockets
-            $dsn = sprintf(
+
+try {
+ $dsn = sprintf(
                 'mysql:dbname=%s;unix_socket=%s',
                 $dbName,
                 $instanceUnixSocket
             );
 
             // Connect to the database.
-            $conn = new PDO(
-                $dsn,
-                $username,
-                $password,
-                # ...
-            );
-        } catch (TypeError $e) {
-            throw new RuntimeException(
-                sprintf(
-                    'Invalid or missing configuration! Make sure you have set ' .
-                        '$username, $password, $dbName, ' .
-                        'and $instanceUnixSocket (for UNIX socket mode). ' .
-                        'The PHP error was %s',
-                    $e->getMessage()
-                ),
-                (int) $e->getCode(),
-                $e
-            );
-        } catch (PDOException $e) {
-            throw new RuntimeException(
-                sprintf(
-                    'Could not connect to the Cloud SQL Database. Check that ' .
-                        'your username and password are correct, that the Cloud SQL ' .
-                        'proxy is running, and that the database exists and is ready ' .
-                        'for use. For more assistance, refer to %s. The PDO error was %s',
-                    'https://cloud.google.com/sql/docs/mysql/connect-external-app',
-                    $e->getMessage()
-                ),
-                (int) $e->getCode(),
-                $e
-            );
-        }
-
-        return $conn;
-    }
+            $conn = new PDO($dsn,$username,$password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
+?>
+            
+ 
