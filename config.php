06c091db7534453ee19dd077a0cf4839fcc736a6
manishayadla <31947122+manishayadla@users.noreply.github.com>
2022-12-07 23:42:40 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,21 +1,14 @@
 <?php
+$servername = "adb-yadlamanisha:us-central1:manishayadla";
+$username = "root";
+$password = "123";
 
-$host = "adb-yadlamanisha:us-central1:manishayadla";
-
-$username = "root"; 
-
-$password = "123"; 
-
-$dbname = "hospitalmanagement"; 
-
-
-
-$conn = new mysqli($host, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-
-    die("Connection failed: " . $conn->connect_error);
-
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=hospitalmanagement", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-
-?> 
+?>
