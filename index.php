225408219107f40ddb7aec3dc26111148a70f4ab
manishayadla <31947122+manishayadla@users.noreply.github.com>
2022-12-08 00:50:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,24 @@ switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
    case '/createDiagnosis.php':
       require 'createDiagnosis.php';
       break;
+   case '/deleteBill.php':
+      require 'deleteBill.php';
+      break;
+   case '/deleteDiagnosis.php':
+      require 'deleteDiagnosis.php';
+      break;   
+   case '/updateBill.php':
+      require 'updateBill.php';
+      break;
+   case '/updateDiagnosis.php':
+      require 'updateDiagnosis.php';
+      break;  
+   case '/viewBill.php':
+      require 'viewBill.php';
+      break;
+   case '/viewDiagnosis.php':
+      require 'viewDiagnosis.php';
+      break;
    default:
       http_response_code(404);
       exit('Not Found');
