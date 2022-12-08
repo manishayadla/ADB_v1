7dd82776dbcd09ed69ba725628c648a7e351f9c7
manishayadla <31947122+manishayadla@users.noreply.github.com>
2022-12-07 22:53:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,16 @@
-<!DOCTYPE html>
-<html>
-   <style>
-      table {
-         border:1px solid black;
-         padding: 0px;
-      }
-      th, td{
-         border:1px solid black;
-         padding: 20px;
-      }
-   </style>
-<body>
-   <h2 align="center">CHOOSE YOUR OPTION</h2>
-   <table style="width: 100%;">
-      <tr align="center">
-         <td><a href="createBill.php">Create Bill</a></td>
-         <td><a href="viewBill.php">View Bill</a></td>
-      </tr>
-      <tr align="center">
-         <td><a href="createDiagnosis.php">Create Diagnosis</a></td>
-         <td><a href="viewDiagnosis.php">View Diagnosis</a></td>
-      </tr>
-   </table>
-</body>
-</html>
+<?php
+switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+   case '/':                   // URL (without file name) to a default screen
+      require 'index1.php';
+      break; 
+   case '/createBill.php':     // if you plan to also allow a URL with the file name 
+      require 'createBill.php';
+      break;              
+   case '/createDiagnosis.php':
+      require 'createDiagnosis.php';
+      break;
+   default:
+      http_response_code(404);
+      exit('Not Found');
+}  
+?>
